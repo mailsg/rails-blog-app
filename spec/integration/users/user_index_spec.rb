5b5914f1152d995a83071696f6dac7fe4ffbd8a2
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-04 18:18:17 +0500

    add integration for user show

--- spec/integration/users/user_index_spec.rb
+++ spec/integration/users/user_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'user show view page', type: :system do
+RSpec.describe 'user index view page', type: :system do
   let!(:user1) do
     User.create(
       name: 'test user1',
@@ -10,42 +10,59 @@ RSpec.describe 'user show view page', type: :system do
     )
   end
 
-  let!(:posts) do
-    [
-      Post.create(author: user1, title: 'Post 1', text: 'Post 1 content', comments_counter: 1, likes_counter: 1),
-      Post.create(author: user1, title: 'Post 2', text: 'Post 2 content', comments_counter: 2, likes_counter: 2),
-      Post.create(author: user1, title: 'Post 3', text: 'Post 3 content', comments_counter: 3, likes_counter: 3)
-    ]
+  let!(:user2) do
+    User.create(
+      name: 'test user2',
+      photo: 'https://images.pexels.com/photos/2913125/pexels-photo-2913125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      bio: 'test_bio2',
+      posts_counter: 2
+    )
   end
 
-  describe 'show user and post details' do
+  let!(:user3) do
+    User.create(
+      name: 'test user3',
+      photo: 'https://static.photocdn.pt/images/articles/2018/12/05/articles/2017_8/beginner_photography_mistakes-1.webp',
+      bio: 'test_bio3',
+      posts_counter: 0
+    )
+  end
+
+  describe 'user content' do
     before(:example) do
-      visit user_path(user1)
+      visit users_path
     end
 
-    it 'displays user information' do
+    it 'can see all users names' do
       expect(page).to have_content(user1.name)
-      expect(page).to have_content(user1.posts_counter)
+      expect(page).to have_content(user2.name)
+      expect(page).to have_content(user3.name)
+    end
+
+    it ' can see profile picture' do
+      expect(page).to have_selector('img#user-image[src="https://images3.alphacoders.com/690/690494.jpg"]')
+      expect(page).to have_selector('img#user-image[src="https://images.pexels.com/photos/2913125/pexels-photo-2913125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]')
+      expect(page).to have_selector('img#user-image[src="https://static.photocdn.pt/images/articles/2018/12/05/articles/2017_8/beginner_photography_mistakes-1.webp"]')
+    end
+
+    it 'can see posts count' do
+      expect(page).to have_content('Number of posts: 1')
+      expect(page).to have_content('Number of posts: 2')
+      expect(page).to have_content('Number of posts: 0')
+    end
+  end
+
+  describe 'click on a user' do
+    before(:example) do
+      visit users_path
+    end
+
+    it 'redirects to user show page when clicking on user name' do
+      user_link = find("a[href='#{user_path(user1)}']")
+      user_link.click
+      expect(page).to have_current_path(user_path(user1))
+      expect(page).to have_content(user1.name)
       expect(page).to have_content(user1.bio)
-      expect(page).to have_selector("img[src='#{user1.photo}']")
-    end
-
-    it 'displays user\'s first 3 posts' do
-      posts.take(3).each do |post|
-        expect(page).to have_content(post.title)
-        expect(page).to have_content(post.text)
-      end
-    end
-
-    it 'go to post show page when clicking a post' do
-      user_links = all("a[href='#{user_post_path(user1, posts.first)}']")
-      user_links.first.click
-      expect(page).to have_current_path(user_post_path(user1, posts.first))
-    end
-
-    it 'redirects to user\'s posts index page when clicking to see all posts' do
-      click_link 'See all posts'
-      expect(page).to have_current_path(user_posts_path(user1))
     end
   end
 end
