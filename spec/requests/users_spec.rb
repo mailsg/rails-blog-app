e73e314ff05f77c8577e301372b3413b58146f12
Sandeep Ghosh <mailsg0912@gmail.com>
2023-09-04 16:23:03 +0530

    Install required dependencies

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'users', type: :request do
+  let!(:user) do
+    User.create(
+      name: 'test user1',
+      photo: 'https://example.com/default-photo.jpg',
+      bio: 'test_bio',
+      posts_counter: 2
+    )
+  end
   describe 'GET /index', type: :request do
     before(:example) { get '/users' }
 
@@ -13,16 +21,18 @@ RSpec.describe 'users', type: :request do
     end
 
     it 'includes correct placeholder text in the response body' do
-      expect(response.body).to include('User')
+      expect(response.body).to include('Number of posts: 2')
     end
   end
   describe 'GET /show', type: :request do
-    user = User.create(
-      name: 'test user',
-      photo: 'https://example.com/photos/0X8086XX09',
-      bio: 'test_bio',
-      posts_counter: 1
-    )
+    let!(:user) do 
+      User.create(
+        name: 'test user',
+        photo: 'https://example.com/photos/0X8086XX09',
+        bio: 'test_bio',
+        posts_counter: 1
+      )
+    end
     before(:example) { get "/users/#{user.id}" }
 
     it 'displays the user details for a given user' do
@@ -34,7 +44,7 @@ RSpec.describe 'users', type: :request do
     end
 
     it 'includes correct placeholder text in the response body' do
-      expect(response.body).to include('User')
+      expect(response.body).to include('test user1')
     end
   end
 end
