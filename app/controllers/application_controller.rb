7f41c94012ab3fe7146ee8390f7cf2c02514fe84
Sandeep Ghosh <mailsg0912@gmail.com>
2023-09-06 12:28:48 +0530

    Install cancancan and add authorization rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
+
+  before_action :load_user_abilities
+
+  private
+
+  def load_user_abilities
+    @load_user_abilities ||= Ability.new(current_user)
+  end
 
   protected
 
