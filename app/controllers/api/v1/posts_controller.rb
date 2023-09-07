27b389f084e0b0f3545b1a8967e292586478ce06
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-07 13:00:52 +0500

    add API post controller and its actions

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::PostsController < ApplicationController
+    before_action :find_user
+
+    def index
+      @posts = @user.posts
+      render json: @posts
+    end
+  
+    private
+  
+    def find_user
+      @user = User.find(params[:user_id])
+    end
+
 end
